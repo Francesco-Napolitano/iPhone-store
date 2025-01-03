16e538c86543b6ded0ba3c5bf7a804886edd8bc3
Francesco-Napolitano <francesco.46napolitano@gmail.com>
2025-01-03 17:23:23 +0100

    ModelView modifiche

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { PerspectiveCamera, View } from '@react-three/drei'
+import Lights from './Lights'
+import { Suspense } from 'react'
+import { div } from 'three/tsl'
 
-const ModelView = () => {
-  return <div></div>
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
+  return (
+    <View
+      index={index}
+      id="gsapType"
+      className={`border-2  border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : ''`}
+    >
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+      <Suspense fallback={<div>Loading...</div>}></Suspense>
+    </View>
+  )
 }
 
 export default ModelView
